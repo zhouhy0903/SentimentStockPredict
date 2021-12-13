5c313d40b0856fc54e0bc8c398d4b1780be54048
zhouhy0903 <zhouhy990903@gmail.com>
2021-12-13 21:53:29 +0800

    crawl in an interval

--- datacollector/wb.py
+++ datacollector/wb.py
@@ -42,61 +42,146 @@ class crawler():
             # return ()
         return wbdate
 
-    def get_page(self,pagenum):
-        # params={'container_ext':'profile_uid:'+self.userid, 'containerid':'100103type=401&q='+self.query,'page_type':'searchall'} if self.query else {'containerid':'107603'+self.userid}
+    def get_page_json(self,pagenum):
         params={'containerid':'107603'+self.userid}
+        # params={'container_ext':'profile_uid:'+self.userid, 'containerid':'100103type=401&q='+self.query,'page_type':'searchall'} if self.query else {'containerid':'107603'+self.userid}
         params['page']=pagenum
         url='https://m.weibo.cn/api/container/getIndex?'
         r=requests.get(url,params=params,headers=self.headers,verify=False)
         # reply=self.testjson()
         # print(r.text)
         reply=r.json()
+        return reply
 
-        # print(reply)
+    def get_page_firsttime(self,pagenum):
+        reply=self.get_page_json(pagenum)
+        if reply['ok']==1:
+            # print(reply['data']['cards'])
+            try:
+                time=self.convertdate(reply['data']['cards'][0]['mblog']['created_at']) if reply['data']['cards'][0].get('mblog') else self.convertdate(reply['data']['cards'][1]['mblog']['created_at'])
+                return time
+            except:
+                print("not find time!") 
+        
+        return None
+
+    def get_page(self,pagenum):
+        reply=self.get_page_json(pagenum)
         wb_list=[]
         if reply['ok']==1:
             # print(reply['data']['cards'])
             cards=reply['data']['cards']
+            
             for card in cards:
-                isLong=card['mblog'].get('isLongText')
-                # print(isLong)
+                # isLong=card['mblog'].get('isLongText')
                 wb={}                
                 # get weibo info
+                wb["wbid"]=card['mblog'].get('id')
                 wb["time"]=self.convertdate(card['mblog']['created_at'])
                 # get text content
-                if isLong:
-                    for _ in range(self.maxtry):
-                        url="https://m.weibo.cn/detail/"+card['mblog']['id']
-                        html=requests.get(url,headers=self.headers,verify=False).text
-                        html = html[html.find('"status":'):html.find("hotScheme")]
-                        html = '{'+html[:html.rfind(',')]+'}'
-                        html_json=json.loads(html,strict=False)
-                        if html_json.get("status"):
-                            html=etree.HTML(html_json["status"]["text"])
-                            wb["text"]=html.xpath('string(.)')       
-                            break
-                        sleep(randint(2,5))
-                else:
-                    text=card['mblog']['text']
-                    html=etree.HTML(text)
-                    wb["text"]=html.xpath('string(.)')       
+                for _ in range(self.maxtry):
+                    url="https://m.weibo.cn/detail/"+card['mblog']['id']
+                    html=requests.get(url,headers=self.headers,verify=False).text
+                    html = html[html.find('"status":'):html.find("hotScheme")]
+                    html = '{'+html[:html.rfind(',')]+'}'
+                    html_json=json.loads(html,strict=False)
+                    if html_json.get("status"):
+                        wb["like"]=html_json["status"]["attitudes_count"]
+                        wb["comment"]=html_json["status"]["comments_count"]
+                        wb["repost"]=html_json["status"]["reposts_count"]
+                        html=etree.HTML(html_json["status"]["text"])
+                        wb["text"]=html.xpath('string(.)')
+                        break
+                    sleep(randint(2,5))
+                
                 wb_list.append(wb)
         else:
             pass
-        self.wb_list=wb_list
+        self.wb_list.extend(wb_list)
         return
+
+    def get_max_page(self):
+        return 100 #待修改
 
     # 用于实现获取指定页数 
     def get_pages(self,startdate,enddate):
-        pass
+        page20=self.get_page_firsttime(20)
+        sleep(randint(1,500)/50)
+        page1=self.get_page_firsttime(1)
+
+        datetime20=datetime.strptime(page20,"%Y-%m-%d %H:%M:%S")
+        datetime1=datetime.strptime(page1,"%Y-%m-%d %H:%M:%S")
+        
+        freq=20//(datetime1-datetime20).days-1
+        
+        datetimestart=datetime.strptime(startdate,"%Y-%m-%d")
+        datetimeend=datetime.strptime(enddate,"%Y-%m-%d")
+        
+
+        left,right=1,self.get_max_page()
+        mid=(left+right)//2        
+        startpage,endpage=0,0
+        while left+3<right:
+            mid=(left+right)//2        
+            print(left,right)
+            midtime=datetime.strptime(self.get_page_firsttime(mid),"%Y-%m-%d %H:%M:%S")
+            if 0<(midtime-datetimeend).days<1:
+                endpage=mid
+                break
+            if midtime<=datetimeend:
+                right=mid
+            else:
+                left=mid
+            sleep(randint(1,500)/50)
+        
+        if left+3>=right:
+            endpage=left
+        endtime=datetime.strptime(self.get_page_firsttime(endpage),"%Y-%m-%d %H:%M:%S")
+        # print(endpage,endtime)
+        
+
+        left,right=1,self.get_max_page()
+        mid=(left+right)//2        
+        while left+3<right:
+            mid=(left+right)//2      
+            print(left,right)  
+            midtime=datetime.strptime(self.get_page_firsttime(mid),"%Y-%m-%d %H:%M:%S")
+            if 0<(datetimestart-midtime).days<1:
+                startpage=mid
+                break
+            if midtime>=datetimestart:
+                left=mid
+            else:
+                right=mid
+            sleep(randint(1,500)/50)
+
+        if left+3>=right:
+            startpage=right
+        starttime=datetime.strptime(self.get_page_firsttime(startpage),"%Y-%m-%d %H:%M:%S")
+        
+        print(startpage,starttime)
+        print(endpage,endtime)
+        print("Crawling from %s to %s pages" % (startpage,endpage))
+        for i in range(startpage,endpage,-1):
+            print("Crawling page %s" % i)
+            self.get_page(i)
+            sleep(randint(1,40)/10)
+
+        
+
+
 
 
 
 if __name__=="__main__":
     # 这里填写需要抓取的用户id和cookie
     mycrawler=crawler("2803301701")
-    mycrawler.get_page(10)
+    startdate="2021-12-03"
+    enddate="2021-12-05"
+
+    mycrawler.get_pages(startdate,enddate)
     wb_dataframe=pd.DataFrame(mycrawler.wb_list)
+    wb_dataframe["title"]=wb_dataframe["text"].apply(lambda x:x[x.find("【"):x.find("】")+1])
     print(wb_dataframe)
     # wb_dataframe.to_csv("test_ren.csv",index=False)
 
